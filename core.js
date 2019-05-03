a2c351433cecd9a62cf183fc37b93646645f4cd4
Frida Pettersen <35762523+Gnusifus@users.noreply.github.com>
2019-05-03 17:20:08 +0200

    Initial commit

---
+++ core.js
@@ -0,0 +1,43 @@
+$(function(){
+  $(".flip-card").on("mouseenter mouseleave", function(){
+    $(this).find(".player-nick").stop().toggle("slow");
+  })
+
+  $(document).on('click', '.cta', function () {
+    $(this).toggleClass('active');
+})
+
+  if (parseInt($(window).width()) < 768) {
+    $("#carousel").not(".card-group").remove();
+    $('#carousel').children().filter(':not(.card-group)').hide();
+}
+
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+      if (scroll >= 340) {
+          if($(".nav-img").length < 1){
+            $(".navbar-brand").before("<a href='#' id='brand-img'><img src='./img/demise_the_D.png' class='d-inline-block align-top nav-img' alt=''></a>");
+          }
+          $(".toggle-btn").css("background-color", "white");
+          $(".toggle-btn:before").css("box-shadow", "0 -10px 0 0 white");
+          $(".toggle-btn:after").css("box-shadow", "0 10px 0 0 white");
+          $(".navbar").css("background-color", "black");
+          $(".navbar").css("border-bottom", "2px solid #E94724");
+          $(".nav-link, .navbar-brand").css("color", "white");
+          $(".navbar").removeClass("navbar-light").addClass("navbar-dark");
+          if(scroll >= 900){
+            $("#take_me_up").show();
+          }
+      }
+      else {
+          $(".navbar").removeClass("navbar-dark").addClass("navbar-light");
+          $(".navbar").css("background-color", "transparent");
+          $(".navbar").css("border-bottom", "none");
+          $(".nav-link, .navbar-brand").css("color", "#231F20");
+          $("#brand-img").remove();
+          $("#take_me_up").hide();
+      }
+});
+
+})
